b9ec1426e0bd3a89f4b59100674869cfb1a07a0b
TKG <orin8622@gmail.com>
2026-02-02 15:33:41 +0900

    modify astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
     site: "https://sim-instagram.vercel.app",
-    output: "server", // ← 重要
+    output: "server",
     adapter: vercel(),
     integrations: [sitemap()],
 });
